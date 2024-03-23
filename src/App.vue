5bad1e898600278797640595cb542c80fcd1c483
Javier Villalba <javier.villalba@cast.com.py>
2024-03-23 19:33:03 -0300

    feat: separate header and footer, add cart functionality

--- src/App.vue
+++ src/App.vue
@@ -1,286 +1,65 @@
 <script setup>
     import { ref, reactive, onMounted } from 'vue';
     import {db} from './data/guitars.js'
+    import Guitar from './components/Guitar.vue'
+    import Header from './components/Header.vue'
+    import Footer from './components/Footer.vue'
 
-    const guitarras = ref([])
+    const guitars = ref([])
+    const cart = ref([])
 
     onMounted(() => {
-        guitarras.value = db
+        guitars.value = db
 
-    })  
+    })
+
+    const addToCart = (guitar) => {
+        const exist = cart.value.findIndex(guitarInCart => guitarInCart.id === guitar.id)
+        if (exist < 0) {
+            guitar.qty = 1
+            cart.value.push(guitar)
+        } else {
+            cart.value.at(exist).qty++
+        }
+    }
+
+    const increaseQuantity = (g) => {
+        g.qty++
+    }
+
+    const decreaseQuantity = (g) => {
+        const current = g.qty
+        if(current === 1){
+            const toRemove = cart.value.findIndex(guitarInCart => guitarInCart.id === g.id)
+            cart.value.splice(toRemove, 1)
+        } else {
+            g.qty--
+        }
+    }
+
 </script>
 
 <template>
-<header class="py-5 header">
-        <div class="container-xl">
-            <div class="row justify-content-center justify-content-md-between">
-                <div class="col-8 col-md-3">
-                    <a href="index.html">
-                        <img class="img-fluid" src="/img/logo.svg" alt="imagen logo">
-                    </a>
-                </div>
-                <nav class="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-                    <div 
-                        class="carrito"
-                    >
-                        <img class="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
-                        <div id="carrito" class="bg-white p-3">
-                            <p class="text-center">El carrito esta vacio</p>
-                            <table class="w-100 table">
-                                <thead>
-                                    <tr>
-                                        <th>Imagen</th>
-                                        <th>Nombre</th>
-                                        <th>Precio</th>
-                                        <th>Cantidad</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <img class="img-fluid" src="/img/guitarra_02.jpg" alt="imagen guitarra">
-                                        </td>
-                                        <td>SRV</td>
-                                        <td class="fw-bold">
-                                                $299
-                                        </td>
-                                        <td class="flex align-items-start gap-4">
-                                            <button
-                                                type="button"
-                                                class="btn btn-dark"
-                                            >
-                                                -
-                                            </button>
-                                                1
-                                            <button
-                                                type="button"
-                                                class="btn btn-dark"
-                                            >
-                                                +
-                                            </button>
-                                        </td>
-                                        <td>
-                                            <button
-                                                class="btn btn-danger"
-                                                type="button"
-                                            >
-                                                X
-                                            </button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-
-                            <p class="text-end">Total pagar: <span class="fw-bold">$899</span></p>
-                            <button class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
-                        </div>
-                    </div>
-                </nav>
-            </div><!--.row-->
-
-            <div class="row mt-5">
-                <div class="col-md-6 text-center text-md-start pt-5">
-                    <h1 class="display-2 fw-bold">Modelo VAI</h1>
-                    <p class="mt-5 fs-5 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, possimus quibusdam dolor nemo velit quo, fuga omnis, iure molestias optio tempore sint at ipsa dolorum odio exercitationem eos inventore odit.</p>
-                    <p class="text-primary fs-1 fw-black">$399</p>
-                    <button 
-                        type="button"
-                        class="btn fs-4 bg-primary text-white py-2 px-5"
-                    >Agregar al Carrito</button>
-                </div>
-            </div>
-        </div>
-
-        <img class="header-guitarra" src="/img/header_guitarra.png" alt="imagen header">
-    </header>
+    <Header 
+        v-bind:cart="cart"
+        v-on:increase-qty="increaseQuantity"
+        v-on:decrease-qty="decreaseQuantity"
+    />
 
     <main class="container-xl mt-5">
         <h2 class="text-center">Nuestra Colección</h2>
 
         <div class="row mt-5">
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="/img/guitarra_01.jpg" alt="imagen guitarra">
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div><!-- FIN GUITARRA -->
-
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="/img/guitarra_02.jpg" alt="imagen guitarra">
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">SRV</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div><!-- FIN GUITARRA -->
-
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="/img/guitarra_03.jpg" alt="imagen guitarra">
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Borland</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div><!-- FIN GUITARRA -->
-
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="/img/guitarra_04.jpg" alt="imagen guitarra">
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Vai</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div><!-- FIN GUITARRA -->
-
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="/img/guitarra_05.jpg" alt="imagen guitarra">
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Thompson</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div><!-- FIN GUITARRA -->
-
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="/img/guitarra_06.jpg" alt="imagen guitarra">
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">White</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div><!-- FIN GUITARRA -->
-
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="/img/guitarra_07.jpg" alt="imagen guitarra">
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Cobain</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div><!-- FIN GUITARRA -->
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="/img/guitarra_08.jpg" alt="imagen guitarra">
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Dale</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div><!-- FIN GUITARRA -->
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="/img/guitarra_09.jpg" alt="imagen guitarra">
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Krieger</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div><!-- FIN GUITARRA -->
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="/img/guitarra_10.jpg" alt="imagen guitarra">
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Campbell</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                                        <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div><!-- FIN GUITARRA -->
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="/img/guitarra_11.jpg" alt="imagen guitarra">
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Reed</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div><!-- FIN GUITARRA -->
-            <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="/img/guitarra_12.jpg" alt="imagen guitarra">
-                </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Hazel</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
-                    <button 
-                        type="button"
-                        class="btn btn-dark w-100 "
-                    >Agregar al Carrito</button>
-                </div>
-            </div><!-- FIN GUITARRA -->
+            <Guitar 
+                v-for="guitar in guitars"
+                v-bind:guitar="guitar"
+                v-on:add-to-cart="addToCart"
+            />
         </div>
     </main>
 
+    <Footer
+    />
 
-    <footer class="bg-dark mt-5 py-5">
-        <div class="container-xl">
-            <p class="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-        </div>
-    </footer>
 </template>
---
+++ src/components/Guitar.vue
@@ -0,0 +1,42 @@
+
+<script setup>
+
+    const props = defineProps({
+        guitar: {
+            type: Object,
+            required: true
+        }
+    })
+
+    defineEmits(['add-to-cart'])
+    
+
+</script>
+
+<template>
+    <div  class="col-md-6 col-lg-4 my-4 row align-items-center">
+        <div class="col-4">
+            <img 
+                class="img-fluid" 
+                v-bind:src="'/img/' + guitar.imagen + '.jpg'" 
+                v-bind:alt="'Guitarra ' + guitar.nombre"
+            >
+        </div>
+        <div class="col-8">
+            <h3 class="text-black fs-4 fw-bold text-uppercase">{{guitar.nombre}}</h3>
+            <p>{{ number }}</p>
+            <p>{{ guitar.descripcion }}</p>
+            <p class="fw-black text-primary fs-3">${{guitar.precio}}</p>
+            <button 
+                type="button"
+                class="btn btn-dark w-100 "
+                
+                v-on:click="$emit('add-to-cart', guitar)"
+                >Agregar al Carrito</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
